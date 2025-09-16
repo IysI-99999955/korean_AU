ae2fed8afa414db689c11900400e28279db1bab0
IysI-99999955 <changwing@naver.com>
2025-09-16 21:45:40 +0900

    fixed a file

--- sh_for_gcp/clone_git_repo.sh
+++ sh_for_gcp/clone_git_repo.sh
@@ -4,5 +4,6 @@
 sudo apt-get update && sudo apt-get install -y curl git vim && echo "curl, git, vim installed."
 
 # GitHub 리포지토리 클론
-read -p "Enter the GitHub repository URL to clone: " GIT_URL
+# read -p "Enter the GitHub repository URL to clone: " GIT_URL
+GIT_URL = "https://github.com/IysI-99999955/korean_AU.git"
 git clone $GIT_URL && echo "Repository cloned from $GIT_URL."
