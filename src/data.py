abf8b1bb5cc1649b9d4773f2fc6e33232502edb3
IysI-99999955 <changwing@naver.com>
2025-09-19 00:13:19 +0900

    return_token_type_ids -activate

--- src/data.py
+++ src/data.py
@@ -46,6 +46,7 @@ def construct_tokenized_dataset(dataset, tokenizer, max_length):
         max_length=max_length,
         add_special_tokens=True,
         # return_token_type_ids=False,  # BERT 이후 모델(RoBERTa 등) 사용할때 False
+        return_token_type_ids=True
     )
     print("tokenizing 된 데이터 형태")
     print("-" * 100)
